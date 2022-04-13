4a6d39ac0dff81ab6f6b0489dc1f26bb363c09f5
Eascaty <63639459+Eascaty@users.noreply.github.com>
2022-04-13 16:58:19 +0800

    毕设代码可运行版v1.1 带数据库

--- src/main/java/com/xiao/crm/annoation/RequiredPermission.java
+++ src/main/java/com/xiao/crm/annoation/RequiredPermission.java
@@ -5,8 +5,8 @@ import java.lang.annotation.*;
 /**
  * 定义方法需要的对应资源的权限码
  */
-@Target({ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface RequiredPermission {
     // 权限码
